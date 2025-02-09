60c275e1ea154dbfea132c5d2e31857acf5ff01c
Vasyl <149408876+Jlemon2012@users.noreply.github.com>
2025-02-09 12:52:28 +0000

    added blocking codes and promises

---
+++ 15-blocking-code.js
@@ -0,0 +1,29 @@
+const http = require('http');
+
+const server = http.createServer(
+    (req, res) => {
+        if (req.url === '/') {
+            res.end('Home Page');
+            return;
+        };
+        if (req.url === '/about') {
+            // BLOCKING CODE !!!!
+            for (let i = 0; i < 1000; i++) {
+                for (let j = 0; j < 1000; j++) {
+                    console.log(`${i},${j}`);
+                };
+            };
+            // BLOCKING CODE !!!!
+            res.end('About Page');
+            return;
+        };
+        res.end('Error Page');
+    }
+);
+
+server.listen(
+    5000, 
+    () => {
+        console.log('Server is listening on port 5000...');
+    }
+);
