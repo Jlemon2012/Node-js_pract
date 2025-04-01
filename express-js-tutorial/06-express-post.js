b95ce9bebdd4b673c8ab8b69a517647b6f6097e0
Vasyl <149408876+Jlemon2012@users.noreply.github.com>
2025-04-01 17:10:56 +0000

    express post method

--- express-js-tutorial/06-express-post.js
+++ express-js-tutorial/06-express-post.js
@@ -4,27 +4,18 @@ let {people} = require('./data');
 
 // static assets
 app.use(express.static('./methods-public'));
-//parse form data
+
+//parse form data, post does not work without this
 app.use(express.urlencoded({extended: false}));
-//parse json
-app.use(express.json());
 
 app.get('/api/people', (req, res) => {
     res.status(200).json({sucess: true, data: people});
 });
 
-app.post('/api/people', (req, res) => {
-    const {name} = req.body;
-    if (!name) {
-        return res.status(401).json({success: false, msg:'please provide name value'});
-    };
-    res.status(201).json({success: true, person: name});
-});
-
 app.post('/login', (req, res) => {
     const {name} = req.body;
     if (name) {
-        return res.status(201).send(`Welcome ${name}`);
+        return res.status(200).send(`Welcome ${name}`);
     };
 
     res.status(401).send('Please Provide Credentials');
